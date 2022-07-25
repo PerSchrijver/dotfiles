605c2779be324e3ccec62e4f87a4b813be80e4ef
MrPerux <perschrijver@gmail.com>
2022-07-25 19:03:03 +0200

    Cleanup fish shell config and add git aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,71 +1,76 @@
-# ~/.config/fish/config.fish
+# Fish config
 
-starship init fish | source
+function fish_greeting # INTERACTIVE ONLY CONFIGURATION
+    # Text editor
+    set -g -x TEXT_EDITOR vim
+    alias vim "nvim -c \"normal Mzt\""
 
-set text_editor vim
-
-# Faust CTF alias
-alias atk=/home/per/Downloads/ataka-player-cli/cli.py
-
-alias vim "nvim -c \"normal Mzt\""
-alias ll "exa -l -g --icons"
-alias lla "ll -a"
-alias config "/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
-config config --local status.showUntrackedFiles no
-alias i "sudo apt install"
-alias diff "diff --color --side-by-side"
-alias bat batcat
-alias s "python3 -i ~/scripts/python_scratchpad.py"
-alias gitdiff "ydiff -s -w0"
-alias p instantiate_project
-alias git "echo \"Don't you mean 'git-version-control-system'?\""
-alias gc "/usr/bin/git commit -m"
-alias ga "/usr/bin/git add"
-alias gap "ga -p"
-alias gaa "ga ."
-alias gac "gaa&&gc"
-alias gs "/usr/bin/git status"
-alias gd "gitdiff"
-alias gds "gd --staged"
-alias git-version-control-system /usr/bin/git
-function instantiate_project
-   ~/Templates/instantiate_project $argv[1..-1] && cd $argv[2]
-end
-
-set -g -x FISHCONFIG ~/.config/fish/config.fish
-set -g -x STARSHIPCONFIG ~/.config/starship.toml
-set -g -x I3CONFIG ~/.config/i3/config
-set -g -x VIMCONFIG ~/.config/nvim/init.vim
-set -g -x TMUXCONFIG ~/.tmux.conf
-set -g -x SSHCONFIG ~/.ssh/config
-
-function fishconfig
-    $text_editor $FISHCONFIG
-    source $FISHCONFIG
-end
-function starshipconfig
-    $text_editor $STARSHIPCONFIG
+    # Premium shit
     starship init fish | source
-end
-function i3config
-    $text_editor $STARSHIPCONFIG
-    i3-msg reload > /dev/null
-end
-function vimconfig
-    $text_editor $VIMCONFIG
-end
-function tmuxconfig
-    $text_editor $TMUXCONFIG
-    tmux source $TMUXCONFIG
-end
-function sshconfig
-    $text_editor $SSHCONFIG
-end
+    alias i "sudo apt install"
+    alias ll "exa -l -g --icons"
+    alias lla "ll -a"
+    alias diff "diff --color --side-by-side"
+    alias bat batcat
+    alias p instantiate_project
 
-function fish_user_key_bindings
-    bind \cr peco_select_history # Ctrl-r
-    bind \cg peco_select_file # Ctrl-f
-    bind -k nul forward-char
-    bind \cy do_thing
-end
+    # Git aliases
+    alias gitdiff "ydiff -s -w0"
+    alias git "echo \"Don't you mean 'git-version-control-system'?\""
+    alias gc "/usr/bin/git commit -m"
+    alias ga "/usr/bin/git add"
+    alias gap "ga -p"
+    alias gaa "ga ."
+    alias gac "gaa&&gc"
+    alias gs "/usr/bin/git status"
+    alias gd "gitdiff"
+    alias gds "gd --staged"
+    alias gcc "/usr/bin/git clone"
+    alias git-version-control-system /usr/bin/git
+
+    # Config locations
+    set -g -x FISHCONFIG ~/.config/fish/config.fish
+    set -g -x STARSHIPCONFIG ~/.config/starship.toml
+    set -g -x I3CONFIG ~/.config/i3/config
+    set -g -x VIMCONFIG ~/.config/nvim/init.vim
+    set -g -x TMUXCONFIG ~/.tmux.conf
+    set -g -x SSHCONFIG ~/.ssh/config
+
+    # Custom functions
+    function instantiate_project
+       ~/Templates/instantiate_project $argv[1..-1] && cd $argv[2]
+    end
+    function nautilushere
+        nautilus . >/dev/null 2>/dev/null &
+    end
+    function fishconfig
+        $TEXT_EDITOR $FISHCONFIG
+        source $FISHCONFIG
+    end
+    function starshipconfig
+        $TEXT_EDITOR $STARSHIPCONFIG
+        starship init fish | source
+    end
+    function i3config
+        $TEXT_EDITOR $STARSHIPCONFIG
+        i3-msg reload > /dev/null
+    end
+    function vimconfig
+        $TEXT_EDITOR $VIMCONFIG
+    end
+    function tmuxconfig
+        $TEXT_EDITOR $TMUXCONFIG
+        tmux source $TMUXCONFIG
+    end
+    function sshconfig
+        $TEXT_EDITOR $SSHCONFIG
+    end
+
+    # Fish keybinds
+    function fish_user_key_bindings
+        bind \cr peco_select_history # Ctrl-r
+        bind \cg peco_select_file # Ctrl-f
+        bind -k nul forward-char
+    end
+end # INTERACTIVE ONLY CONFIGURATION
 
